0b7a8f15088c47ef50fda526940e07431f974e17
tonyzzp <tonyzzp@qq.com>
2022-12-02 18:29:55 +0800

    signal

---
+++ signal/demo_signal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    auto onsig = [](int sig) -> void
+    {
+        printf("onsig: %d\n", sig);
+        exit(0);
+    };
+
+    signal(SIGABRT, onsig);
+    signal(SIGFPE, onsig);
+    signal(SIGILL, onsig);
+    signal(SIGINT, onsig);
+    signal(SIGSEGV, onsig);
+    signal(SIGTERM, onsig);
+
+    printf("press ctrl+c to interrupt\n");
+
+    int i = 0;
+    while (true)
+    {
+        printf("%d\n", ++i);
+        _sleep(1000);
+        if (i == 3)
+        {
+            raise(SIGTERM);
+        }
+    }
+    
+    return 0;
+}
