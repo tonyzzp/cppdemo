2caa3c1c89f69eeb5f8ea33f59ae61671c53be85
tonyzzp <tonyzzp@qq.com>
2022-12-06 15:52:30 +0800

    thread

--- thread/demo_thread.cpp
+++ thread/demo_thread.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <thread>
+#include <stdlib.h>
+#include <memory>
+#include <memory.h>
 
 int main()
 {
@@ -8,13 +11,31 @@ int main()
         std::cout << "fun1.run" << std::endl;
         for (int i = 0; i < 10; i++)
         {
-            std::cout << i << std::endl;
+            std::cout << "fun1:" << i << std::endl;
             _sleep(500);
         }
         std::cout << "fun1.end" << std::endl;
     };
     std::thread t1(fun1, 123);
+
+    pthread_t t2 = 0;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    auto fun2 = [](void *p) -> void *
+    {
+        std::cout << "fun2.run" << std::endl;
+        for (int i = 0; i < 10; i++)
+        {
+            std::cout << "fun2:" << i << std::endl;
+            _sleep(500);
+        }
+        std::cout << "fun2.end" << std::endl;
+        return NULL;
+    };
+    pthread_create(&t2, &attr, fun2, NULL);
+
     t1.join();
+    pthread_join(t2, NULL);
 
     return 0;
 }
